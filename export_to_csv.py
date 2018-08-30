80d370efa36e02892f03fc345a75391f9ed2920c
Assvin <assvin@toladata.com>
2018-08-30 16:56:53 +0200

    Added the function which gives the percentage for countries and removed the workflowlevel1 in the queries

--- export_to_csv.py
+++ export_to_csv.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
 
 
     #We query the database to get each column we need for the orgranization
-    cur.execute("select a.id from workflow_workflowlevel2 a join workflow_workflowlevel1 b on b.id=a.workflowlevel1_id join workflow_organization c on b.organization_id=c.id join workflow_stakeholder_workflowlevel1 d on d.workflowlevel1_id=b.id join workflow_stakeholder e on e.id=d.stakeholder_id where c.name={} and b.name='Humanitarian Response to the Syrian Crisis' order by a.id".format(org))
+    cur.execute("select a.id from workflow_workflowlevel2 a join workflow_workflowlevel1 b on b.id=a.workflowlevel1_id join workflow_organization c on b.organization_id=c.id join workflow_stakeholder_workflowlevel1 d on d.workflowlevel1_id=b.id join workflow_stakeholder e on e.id=d.stakeholder_id where c.name={} order by a.id".format(org))
     ID = [row[0] for row in cur.fetchall()]
     ID2=[]
     #equals to Org Identifier + - + WFL2 ID
@@ -37,45 +37,45 @@ if __name__ == '__main__':
 
 
     #No Currencies in "workflow_currency" table
-    cur.execute("select c.default_currency_id as currency from workflow_workflowlevel2 a join workflow_workflowlevel1 b on b.id=a.workflowlevel1_id join workflow_organization c on b.organization_id=c.id join workflow_stakeholder_workflowlevel1 d on d.workflowlevel1_id=b.id join workflow_stakeholder e on e.id=d.stakeholder_id where c.name= {} and b.name='Humanitarian Response to the Syrian Crisis' order by a.id".format(org))
+    cur.execute("select c.default_currency_id as currency from workflow_workflowlevel2 a join workflow_workflowlevel1 b on b.id=a.workflowlevel1_id join workflow_organization c on b.organization_id=c.id join workflow_stakeholder_workflowlevel1 d on d.workflowlevel1_id=b.id join workflow_stakeholder e on e.id=d.stakeholder_id where c.name= {}order by a.id".format(org))
     currency = [row[0] for row in cur.fetchall()]
 
-    cur.execute("select a.name as workflowlevel2 from workflow_workflowlevel2 a join workflow_workflowlevel1 b on b.id=a.workflowlevel1_id join workflow_organization c on b.organization_id=c.id join workflow_stakeholder_workflowlevel1 d on d.workflowlevel1_id=b.id join workflow_stakeholder e on e.id=d.stakeholder_id where c.name={} and b.name='Humanitarian Response to the Syrian Crisis' order by a.id".format(org))
+    cur.execute("select a.name as workflowlevel2 from workflow_workflowlevel2 a join workflow_workflowlevel1 b on b.id=a.workflowlevel1_id join workflow_organization c on b.organization_id=c.id join workflow_stakeholder_workflowlevel1 d on d.workflowlevel1_id=b.id join workflow_stakeholder e on e.id=d.stakeholder_id where c.name={}order by a.id".format(org))
     WFL2 = [row[0] for row in cur.fetchall()]
     delete_replicate(ID2, WFL2)
 
     #Description is nearly empty in TolaData so I fill up with dummy data if it is empty as it is a required field
-    cur.execute("select a.description as workflowlevel2 from workflow_workflowlevel2 a join workflow_workflowlevel1 b on b.id=a.workflowlevel1_id join workflow_organization c on b.organization_id=c.id join workflow_stakeholder_workflowlevel1 d on d.workflowlevel1_id=b.id join workflow_stakeholder e on e.id=d.stakeholder_id where c.name={} and b.name='Humanitarian Response to the Syrian Crisis' order by a.id".format(org))
+    cur.execute("select a.description as workflowlevel2 from workflow_workflowlevel2 a join workflow_workflowlevel1 b on b.id=a.workflowlevel1_id join workflow_organization c on b.organization_id=c.id join workflow_stakeholder_workflowlevel1 d on d.workflowlevel1_id=b.id join workflow_stakeholder e on e.id=d.stakeholder_id where c.name={}order by a.id".format(org))
     description = [row[0] for row in cur.fetchall()]
     for i in range(len(ID2)):
         if(description[i]==''):
             description[i]='add description'+str(i)
     delete_replicate(ID2, description)
 
-    cur.execute("select a.effect_or_impact as workflowlevel2 from workflow_workflowlevel2 a join workflow_workflowlevel1 b on b.id=a.workflowlevel1_id join workflow_organization c on b.organization_id=c.id join workflow_stakeholder_workflowlevel1 d on d.workflowlevel1_id=b.id join workflow_stakeholder e on e.id=d.stakeholder_id where c.name={} and b.name='Humanitarian Response to the Syrian Crisis' order by a.id".format(org))
+    cur.execute("select a.effect_or_impact as workflowlevel2 from workflow_workflowlevel2 a join workflow_workflowlevel1 b on b.id=a.workflowlevel1_id join workflow_organization c on b.organization_id=c.id join workflow_stakeholder_workflowlevel1 d on d.workflowlevel1_id=b.id join workflow_stakeholder e on e.id=d.stakeholder_id where c.name={}order by a.id".format(org))
     objectives = [row[0] for row in cur.fetchall()]
 
-    cur.execute("select a.status as workflowlevel2 from workflow_workflowlevel2 a join workflow_workflowlevel1 b on b.id=a.workflowlevel1_id join workflow_organization c on b.organization_id=c.id join workflow_stakeholder_workflowlevel1 d on d.workflowlevel1_id=b.id join workflow_stakeholder e on e.id=d.stakeholder_id where c.name={} and b.name='Humanitarian Response to the Syrian Crisis' order by a.id".format(org))
+    cur.execute("select a.status as workflowlevel2 from workflow_workflowlevel2 a join workflow_workflowlevel1 b on b.id=a.workflowlevel1_id join workflow_organization c on b.organization_id=c.id join workflow_stakeholder_workflowlevel1 d on d.workflowlevel1_id=b.id join workflow_stakeholder e on e.id=d.stakeholder_id where c.name={}order by a.id".format(org))
     status = [row[0] for row in cur.fetchall()]
 
 
-    cur.execute("select to_char(a.expected_start_date, 'YYYY/MM/DD') as actual_date from workflow_workflowlevel2 a join workflow_workflowlevel1 b on b.id=a.workflowlevel1_id join workflow_organization c on b.organization_id=c.id join workflow_stakeholder_workflowlevel1 d on d.workflowlevel1_id=b.id join workflow_stakeholder e on e.id=d.stakeholder_id where c.name={} and b.name='Humanitarian Response to the Syrian Crisis' order by a.id".format(org))
+    cur.execute("select to_char(a.expected_start_date, 'YYYY/MM/DD') as actual_date from workflow_workflowlevel2 a join workflow_workflowlevel1 b on b.id=a.workflowlevel1_id join workflow_organization c on b.organization_id=c.id join workflow_stakeholder_workflowlevel1 d on d.workflowlevel1_id=b.id join workflow_stakeholder e on e.id=d.stakeholder_id where c.name={}order by a.id".format(org))
     wfl2_start_date = [row[0] for row in cur.fetchall()]
 
 
-    cur.execute("select to_char(a.expected_end_date, 'YYYY/MM/DD') as end_date from workflow_workflowlevel2 a join workflow_workflowlevel1 b on b.id=a.workflowlevel1_id join workflow_organization c on b.organization_id=c.id join workflow_stakeholder_workflowlevel1 d on d.workflowlevel1_id=b.id join workflow_stakeholder e on e.id=d.stakeholder_id where c.name={} and b.name='Humanitarian Response to the Syrian Crisis' order by a.id".format(org))
+    cur.execute("select to_char(a.expected_end_date, 'YYYY/MM/DD') as end_date from workflow_workflowlevel2 a join workflow_workflowlevel1 b on b.id=a.workflowlevel1_id join workflow_organization c on b.organization_id=c.id join workflow_stakeholder_workflowlevel1 d on d.workflowlevel1_id=b.id join workflow_stakeholder e on e.id=d.stakeholder_id where c.name={}order by a.id".format(org))
     wfl2_end_date = [row[0] for row in cur.fetchall()]
 
 
     #Giving activity status code there are 6 in total but I tried to give a coherent status code
     status2 = []
     for i in range(len(wfl2_end_date)):
-        if((wfl2_end_date[i]!=None) and compare_date(wfl2_end_date[i])):
-            status2.append('3')
-        elif(status[i]=='green'):
-            status2.append('2')
-        else:
-            status2.append('1')
+        #if((wfl2_end_date[i]!=None) and compare_date(wfl2_end_date[i])):
+        #    status2.append('3')
+        #elif(status[i]=='green'):
+        #    status2.append('2')
+        #else:
+            status2.append('1') #putting default status to 1
     delete_replicate(ID2, status2)
 
     #budget_start_date = wfl2_start_date.copy()
@@ -85,25 +85,26 @@ if __name__ == '__main__':
     delete_replicate(ID2, wfl2_start_date)
     delete_replicate(ID2, wfl2_end_date)
 
-    cur.execute("select e.role as stakeholder_role from workflow_workflowlevel2 a join workflow_workflowlevel1 b on b.id=a.workflowlevel1_id join workflow_organization c on b.organization_id=c.id join workflow_stakeholder_workflowlevel1 d on d.workflowlevel1_id=b.id join workflow_stakeholder e on e.id=d.stakeholder_id where c.name={} and b.name='Humanitarian Response to the Syrian Crisis'order by a.id".format(org))
+    cur.execute("select e.role as stakeholder_role from workflow_workflowlevel2 a join workflow_workflowlevel1 b on b.id=a.workflowlevel1_id join workflow_organization c on b.organization_id=c.id join workflow_stakeholder_workflowlevel1 d on d.workflowlevel1_id=b.id join workflow_stakeholder e on e.id=d.stakeholder_id where c.name={} order by a.id".format(org))
     stakeholder_role = [row[0] for row in cur.fetchall()]
     org_role='1'
 
-    cur.execute("select f.name as stakeholder_type from workflow_workflowlevel2 a join workflow_workflowlevel1 b on b.id=a.workflowlevel1_id join workflow_organization c on b.organization_id=c.id join workflow_stakeholder_workflowlevel1 d on d.workflowlevel1_id=b.id left join workflow_stakeholder e on e.id=d.stakeholder_id left join workflow_stakeholdertype f on f.id=e.type_id  where c.name={} and b.name='Humanitarian Response to the Syrian Crisis'order by a.id".format(org))
+    cur.execute("select f.name as stakeholder_type from workflow_workflowlevel2 a join workflow_workflowlevel1 b on b.id=a.workflowlevel1_id join workflow_organization c on b.organization_id=c.id join workflow_stakeholder_workflowlevel1 d on d.workflowlevel1_id=b.id left join workflow_stakeholder e on e.id=d.stakeholder_id left join workflow_stakeholdertype f on f.id=e.type_id  where c.name={} order by a.id".format(org))
     stakeholder_type = [row[0] for row in cur.fetchall()]
 
-    cur.execute("select e.name as stakeholder from workflow_workflowlevel2 a join workflow_workflowlevel1 b on b.id=a.workflowlevel1_id join workflow_organization c on b.organization_id=c.id left join workflow_stakeholder_workflowlevel1 d on d.workflowlevel1_id=b.id left join workflow_stakeholder e on e.id=d.stakeholder_id where c.name={} and b.name='Humanitarian Response to the Syrian Crisis'order by a.id".format(org))
+    cur.execute("select e.name as stakeholder from workflow_workflowlevel2 a join workflow_workflowlevel1 b on b.id=a.workflowlevel1_id join workflow_organization c on b.organization_id=c.id  join workflow_stakeholder_workflowlevel1 d on d.workflowlevel1_id=b.id left join workflow_stakeholder e on e.id=d.stakeholder_id where c.name={} order by a.id".format(org))
     stakeholder = [row[0] for row in cur.fetchall()]
 
 
-    cur.execute("select f.country from workflow_workflowlevel2 a join workflow_workflowlevel1 b on b.id=a.workflowlevel1_id join workflow_organization c on b.organization_id=c.id join workflow_stakeholder_workflowlevel1 d on d.workflowlevel1_id=b.id join workflow_stakeholder e on e.id=d.stakeholder_id join workflow_country f on f.id=e.country_id where c.name={} and b.name='Humanitarian Response to the Syrian Crisis'order by a.id".format(org))
+    cur.execute("select f.country from workflow_workflowlevel2 a join workflow_workflowlevel1 b on b.id=a.workflowlevel1_id join workflow_organization c on b.organization_id=c.id join workflow_stakeholder_workflowlevel1 d on d.workflowlevel1_id=b.id join workflow_stakeholder e on e.id=d.stakeholder_id join workflow_country f on f.id=e.country_id where c.name={} order by a.id".format(org))
     country = [row[0] for row in cur.fetchall()]
     countrycode=[]
     #regioncode=[]
     for c in country:
         countrycode.append(country_code(c))
-
-    percentage_recipient_country = percentage_recipient_country(ID2)
+    country_percentage = []
+    (countrycode,country_percentage) = percentage_recipient_country(countrycode,ID2)
+    #percentage_recipient_country = percentage_recipient_country(ID2)
 
      #for c in country:
     #    regioncode.append(country_to_region(c))
@@ -119,11 +120,12 @@ if __name__ == '__main__':
         budget_status.append('1')
     delete_replicate(ID2, budget_status)
 
-    cur.execute("select a.total_estimated_budget as budget from workflow_workflowlevel2 a join workflow_workflowlevel1 b on b.id=a.workflowlevel1_id join workflow_organization c on b.organization_id=c.id join workflow_stakeholder_workflowlevel1 d on d.workflowlevel1_id=b.id join workflow_stakeholder e on e.id=d.stakeholder_id where c.name={} and b.name='Humanitarian Response to the Syrian Crisis' order by a.id".format(org))
+    cur.execute("select a.total_estimated_budget as budget from workflow_workflowlevel2 a join workflow_workflowlevel1 b on b.id=a.workflowlevel1_id join workflow_organization c on b.organization_id=c.id join workflow_stakeholder_workflowlevel1 d on d.workflowlevel1_id=b.id join workflow_stakeholder e on e.id=d.stakeholder_id where c.name={} order by a.id".format(org))
     budget = [row[0] for row in cur.fetchall()]
     delete_replicate(ID2, budget)
 
     conn.close()
+
 
     #Write the CSV File with the name of the columns from IATI Standard
     counter = 0
@@ -135,5 +137,5 @@ if __name__ == '__main__':
         #Fill up the CSV by adding each column
         max_counter = len(ID)
         while counter < max_counter:
-            filewriter.writerow((ID2[counter], currency[counter],'','', WFL2[counter],description[counter],objectives[counter],'','',status2[counter],wfl2_start_date[counter],wfl2_end_date[counter],'','',org_role,stakeholder_type[counter],stakeholder[counter],'',countrycode[counter],percentage_recipient_country[counter],'','',policy_marker_vocabulary,policy_marker_code,'','','','','','',budget_status[counter],budget_start_date[counter],budget_end_date[counter],budget[counter],budget_start_date[counter] ))
+            filewriter.writerow((ID2[counter], currency[counter],'','', WFL2[counter],description[counter],objectives[counter],'','',status2[counter],wfl2_start_date[counter],wfl2_end_date[counter],'','',org_role,stakeholder_type[counter],stakeholder[counter],'',countrycode[counter],country_percentage[counter],'','',policy_marker_vocabulary,policy_marker_code,'','','','','','',budget_status[counter],budget_start_date[counter],budget_end_date[counter],budget[counter],budget_start_date[counter] ))
             counter = counter +1
--- percentage_recipient_country.py
+++ percentage_recipient_country.py
@@ -1,16 +1,33 @@
 #This function allows to fill in the Recipient Country Percentage column in the csv file
 
-def percentage_recipient_country(array):
-    array1=[]
-    array2=[]
-    for i in range(len(array)):
-        if(array[i]!=''):
+
+def percentage_recipient_country(countries,ID):
+    #get an array of #of rows for each activities
+    count_ID=[]
+    for i in range(len(ID)):
+        if(ID[i]!=''):
             j=1
-            while(i+j<len(array) and array[i+j]==''):
+            while(i+j<len(ID) and ID[i+j]==''):
                 j=j+1
-            array1.append(j)
-    for i in range(len(array1)):
-        n=100/float(int(array1[i]))
-        for j in range(array1[i]):
-            array2.append(str(round(n,4)))
-    return array2
+            count_ID.append(j)
+    #setting the countries and the percentage for each activity
+    k=0
+    percentage=[]
+    sorted_countries=[]
+    for count in count_ID:
+        tab = countries[k:k+count]
+        k=k+count
+        tab2=set(tab)
+        for c in tab2:
+            sorted_countries.append(c)
+            percent = 0
+            for c2 in tab:
+                if (c==c2):
+                    percent=percent+1
+            percent = round(percent/count*100,2)
+            percentage.append(percent)
+        for i in range(count-len(tab2)):
+            sorted_countries.append('')
+            percentage.append('')
+        #k=k+count
+    return (sorted_countries,percentage)
